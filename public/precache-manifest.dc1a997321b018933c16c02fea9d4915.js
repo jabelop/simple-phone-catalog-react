bf2b519181e67fb0990a9be16ca00619e4dbec84
julio abejar <jabelop@hotmail.com>
2020-09-06 13:56:26 +0200

    - Public folder with react build added

---
+++ public/precache-manifest.dc1a997321b018933c16c02fea9d4915.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1451221c39c1d46784c8d9902576ae3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "011b63fac0e79cdbbc2b",
+    "url": "/static/js/2.bf3251f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bf3251f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff4d1a6aa3055a9b2e4f",
+    "url": "/static/js/main.68ea768d.chunk.js"
+  },
+  {
+    "revision": "56c488749967110d6c4b",
+    "url": "/static/js/runtime-main.6c6b63c4.js"
+  }
+]);
